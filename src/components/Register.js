429c7fcb41e2af9374728f8d1bf72a9737317f2f
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2022-04-27 05:02:11 -0400

    fixing register

--- src/components/Register.js
+++ src/components/Register.js
@@ -21,9 +21,7 @@ const Register = ({setUser}) => {
         }
         try {
             const info = await fetchRegister(email, username, password);
-            if(info.message) {
-                setError(info.message)
-            }
+            setError(info.message)
             localStorage.setItem("token", info.token);
 			setEmail("");
 			setUsername("");
