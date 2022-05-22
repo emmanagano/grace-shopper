fdb93e0de22fe3de383640c3c48b286724e16980
Emma Ruth Nagaño <emmaruthnagano@Emmas-MacBook-Pro.local>
2022-05-22 00:00:35 -0400

    deploying

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
-// const BASE_URL = `https://intense-crag-00374.herokuapp.com/api`;
-const BASE_URL = `http://localhost:4000/api`;
+const BASE_URL = `https://intense-crag-00374.herokuapp.com/api`;
+// const BASE_URL = `http://localhost:4000/api`;
 const lstoken = localStorage.getItem("token");
 
 //USER
